ec13bbe5a3e523c95ef205c808d15caf526c42ad
elenfe <elenfe7@gmail.com>
2016-04-21 10:00:58 -0300

    calcula imc js

--- projeto/calcula-imc.js
+++ projeto/calcula-imc.js
@@ -1,18 +1,20 @@
 // p / altura * altura
 
-var tdPeso = document.getElementById(peso-2);
-var tdAltura = document.getElementById(altura-2);
+var tdPeso = document.getElementById("peso-2");
+var tdAltura = document.getElementById("altura-2");
+console.log(tdAltura);
 
 var peso = tdPeso.textContent;
-var altura = tdAltura.get;
+var altura = tdAltura.textContent;
 
 //var ehDiferenteDeZero = (altura != 0); //false
 
 if(altura != 0){
     var imc = peso / (altura * altura); // 25
 
-alert(imc);
-}else{
-    alert("Não executei proque a altura é igual a zero");
+  console.log(imc);
+}else
+{
+    console.log("Não executei proque a altura é igual a zero");
 }
 
